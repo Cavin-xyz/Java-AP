8bdd2cd2b0847d51760ff7247115d687bcaff5ad
Cavin-xyz <cavinxp@gmail.com>
2026-06-23 12:05:01 +0530

    matrix snake pattern filling

--- snake.java
+++ snake.java
@@ -1,28 +1,28 @@
-public class snake{
-        public static void main(String args[]){
-            int rows = 5;
-            int cols = 5;
-            int min = 1;
+public class snake {
+    public static void main(String args[]) {
+        int rows = 5;
+        int cols = 5;
+        int min = 1;
 
-            int[][] matrix = new int [rows][cols];
-            
-            for(int i=0;i<rows;i++){
-                if(i%2 ==0){
-                    for(int j=0;j<cols;j++){
-                        matrix[i][j]=min++;
-                    }
-                } else {
-                    for(int j = cols-1;j>=0;j--){
-                        matrix[i][j]=min++;
-                    }
+        int[][] matrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            if (i % 2 == 0)
+                for (int j = 0; j < cols; j++) {
+                    matrix[i][j] = min++;
+                }
+            else {
+                for (int j = cols - 1; j >= 0; j--) {
+                    matrix[i][j] = min++;
                 }
             }
+        }
 
-              for(int i=0;i<rows;i++){
-                  for(int j=0;j<cols;j++){
-                      System.out.printf("%-3d", matrix[i][j]);
-                    }
-                    System.out.println();
-                }
-          }
-  }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("%-3d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
