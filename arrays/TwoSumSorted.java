73cdb66d895367939bf024419a3ec19de4679242
Cavin-xyz <cavinxp@gmail.com>
2026-08-02 20:03:04 +0530

    two sum (two pointers)

---
+++ arrays/TwoSumSorted.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class TwoSumSorted {
+    // Assumes the array is sorted ascending
+    public static int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[] { left, right };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+
+        Arrays.sort(nums); // ensure sorted for two-pointer approach
+        int[] ans = twoSum(nums, target);
+        System.out.println(ans[0] + " " + ans[1]);
+        sc.close();
+    }
+}
